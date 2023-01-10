bfac46500567b8de776d8295fda12f3953a49077
Juan Pablo <jpccdaw@@gmail.com>
2023-01-10 20:44:51 +0000

    index.js

--- public/controller/index.view.js
+++ public/controller/index.view.js
@@ -7,9 +7,11 @@ title.textContent = "Seleccione una opción";
 ///
 /// CONSTRUCTOR DE INPUT PARA SONG
 ///
+
+//formulario para el input de song
 const form_songs = document.createElement("form");
 form_songs.id = "form_songs";
-
+//constructor del input de song
 const songs = document.createElement("div");
 songs.id = "songs";
 const song = document.createElement("h3");
@@ -18,9 +20,20 @@ song.id = "song";
 song.textContent = "Crea una cancion";
 create_song.type = "text";
 songs.append(song, create_song);
-// form_songs.action = "http://127.0.0.1:5500/api/songs/"+song.nodeValue;
-const link = "http:127.0.0.1:5500/api/songs/" + song.nodeValue;
 
+form_songs.action = "http://127.0.0.1:5500/api/songs/"+create_song.value;
+
+//evento para recoger el valor del input
+create_song.addEventListener("submit", function() {
+fetch("http://127.0.0.1:5500/api/songs/" + create_song.value, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(function(response) {
+    console.log(response);
+})
+})
 ///
 /// CONSTRUCTOR DE INPUT PARA PLAYLIST
 ///
@@ -35,12 +48,12 @@ const create_playlist = document.createElement("input");
 playlist.id = "playlist";
 playlist.textContent = "Crea una playlist";
 create_playlist.type = "text";
-playlists.appendChild(playlist, create_playlist);
+playlists.append(playlist, create_playlist);
 
 const div_input = document.createElement("div");
 
 form_songs.appendChild(songs);
-form_playlist.appendChild(playlist);
+form_playlist.appendChild(playlists);
 
 div_input.append(form_songs, form_playlist);
 
