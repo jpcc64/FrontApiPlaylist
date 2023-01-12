7c0610974895ae751e65e83793d69ada0867f63d
Juan Pablo <jpccdaw@@gmail.com>
2023-01-12 18:50:04 +0000

    haciendo formulario POST

--- public/controller/index.view.js
+++ public/controller/index.view.js
@@ -45,11 +45,10 @@ const pinta_song = (data) => {
   name_songHTML.textContent = data.name;
   const artist_songHTML = document.createElement("h2");
   artist_songHTML.id = "artist_songHTML";
-  artist_songHTML.innerHTML = `<h2 style="background-color: cornflowerblue;color:aliceblue;">${data.artist}</h2>`;
-  song.innerHTML = `<h3 style ="color:red">Cancion encontrada</h3>`
-  create_songHTML.remove()
+  artist_songHTML.innerHTML = `<h2 style="background-color: cornflowerblue;color:aliceblue;">f${data.artist}</h2>`;
+  song.innerHTML = `<h3 style ="color:red">Cancion encontrada</h3>`;
+  create_songHTML.remove();
   songsHTML.append(name_songHTML, artist_songHTML);
-
 };
 ///
 /// CONSTRUCTOR DE INPUT PARA PLAYLIST
@@ -70,9 +69,7 @@ playlistHTML.append(playlist, create_playlistHTML);
 //consulta a la api
 
 const playlist_search = async () => {
-  await fetch(
-    "http://localhost:8080/api/playlist/" + create_playlistHTML.value
-  )
+  await fetch("http://localhost:8080/api/playlist/" + create_playlistHTML.value)
     .then((response) => {
       const json = response.json();
       return json;
@@ -88,10 +85,10 @@ const pinta_playlist = (data) => {
   console.log(data);
   const playlist_songHTML = document.createElement("h2");
   playlist_songHTML.id = "name_songHTML";
-  playlist_songHTML.innerHTML ="La playlist se llama: "+ data.name;
+  playlist_songHTML.innerHTML = "La playlist se llama: " + data.name;
   const tamaño_playlistHTML = document.createElement("h2");
   tamaño_playlistHTML.id = "tamaño_playlistHTML";
-  tamaño_playlistHTML.innerHTML ="Numero de canciones: "+ data.songs.length;
+  tamaño_playlistHTML.innerHTML = "Numero de canciones: " + data.songs.length;
 
   const pFrag = document.createDocumentFragment();
   for (let i = 0; i < data.songs.length; i++) {
@@ -99,11 +96,66 @@ const pinta_playlist = (data) => {
     li.innerHTML = data.songs[i].name;
     pFrag.appendChild(li);
   }
-    playlistHTML.append(playlist_songHTML, tamaño_playlistHTML, pFrag);
+  playlistHTML.append(playlist_songHTML, tamaño_playlistHTML, pFrag);
+};
+///
+/// CONSTRUCTOR DE FORMULARIO POST
+///
+const form_postHTML = document.createElement("form");
+form_postHTML.id = "form_postHTML";
+form_postHTML.method = "POST";
+const postHTML = document.createElement("div");
+postHTML.id = "postHTML";
+const post = document.createElement("h3");
+const createArtistHTML = document.createElement("input");
+const createNameSongHTML = document.createElement("input");
+const creatPlaylisForSongtHTML = document.createElement("input");
+const nombreCancionHTML = document.createElement("h3");
+const artsitaCancionHTML = document.createElement("h3");
+const playlistCancionHTML = document.createElement("h3");
+post.id = "post";
+post.textContent = "Crea una nueva cancion";
+createArtistHTML.type = "text";
+createNameSongHTML.type = "text";
+createNameSongHTML.insertBefore(nombreCancionHTML, createNameSongHTML);
+createArtistHTML.insertBefore(artsitaCancionHTML, createArtistHTML);
+creatPlaylisForSongtHTML.insertBefore(
+  playlistCancionHTML,
+  creatPlaylisForSongtHTML
+);
+postHTML.append(
+  post,
+  createArtistHTML,
+  createNameSongHTML,
+  creatPlaylisForSongtHTML
+);
 
-  
+const pinta_post = (data) => {
+  const idSong = document.createElement("h2");
+  idSong.id = "idSong";
+  idSong.innerHTML = "ID de la cancion: " + data._id;
+  postHTML.append(idSong);
 };
 
+const creaCancion = async () => {
+  let data = {
+    name: createNameSongHTML.value,
+    artist: createArtistHTML.value,
+    playlist: creatPlaylisForSongtHTML.value,
+  };
+  await fetch("http://localhost:8080/api/songs/", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      pinta_post(json);
+    });
+};
 ///
 /// CONSTRUCTOR DE LA PAGINA
 ///
@@ -113,14 +165,15 @@ const div_input = document.createElement("div");
 form_songsHTML.appendChild(songsHTML);
 form_playlistHTML.appendChild(playlistHTML);
 
-div_input.append(form_songsHTML, form_playlistHTML);
- // añade titulo y formularios
-body.appendChild(titleHTML);
-body.appendChild(div_input);
-
 const despliegaPrograma = () => {
+  div_input.append(form_songsHTML, form_playlistHTML);
+  // añade titulo y formularios
+  body.appendChild(titleHTML);
+  body.appendChild(div_input);
+  body.appendChild(postHTML);
   create_songHTML.addEventListener("change", song_search);
   create_playlistHTML.addEventListener("change", playlist_search);
+  creatPlaylisForSongtHTML.addEventListener("change", creaCancion);
 };
 
 despliegaPrograma();
